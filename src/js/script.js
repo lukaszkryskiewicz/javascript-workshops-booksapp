bcfcc727c42f331dfddf51589e562e8e4d7ccbc2
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2022-11-11 18:40:07 +0100

    Exercis 10.4

--- src/js/script.js
+++ src/js/script.js
@@ -9,51 +9,51 @@
         all: {
             booksImages: '.book__image',
         }
-    }
+    };
 
     const classNames = {
         books: {
             favorite: 'favorite',
         }
-    }
+    };
 
     const templates = {
         booksList: Handlebars.compile(document.querySelector(select.templateOf.bookList).innerHTML),
-    }
+    };
     const favoriteBooks = [];
+    const booksListContainer = document.querySelector(select.containerOf.booksList);
 
     function render() {
-        for (book of dataSource.books) {
+        for (const book of dataSource.books) {
 
             const generatedHTML = templates.booksList(book);
             const element = utils.createDOMFromHTML(generatedHTML);
-            const booksListContainer = document.querySelector(select.containerOf.booksList);
             booksListContainer.appendChild(element);
         }
     }
 
     function initActions() {
-        const booksImagesDom = document.querySelectorAll(select.all.booksImages);
-        for (const bookImage of booksImagesDom) {
-            bookImage.addEventListener('dblclick', function (event) {
-                event.preventDefault();
-                const bookImageDataId = bookImage.getAttribute('data-id');
+        booksListContainer.addEventListener('dblclick', function (event) {
+            event.preventDefault();
+            if (event.target.offsetParent.classList.contains('book__image')) {
+                const bookImageDataId = event.target.offsetParent.getAttribute('data-id');
                 if (favoriteBooks.indexOf(bookImageDataId) == -1) {
-                    bookImage.classList.add(classNames.books.favorite);
+                    event.target.offsetParent.classList.add(classNames.books.favorite);
                     favoriteBooks.push(bookImageDataId);
                 } else {
-                    bookImage.classList.remove(classNames.books.favorite);
+                    event.target.offsetParent.classList.remove(classNames.books.favorite);
                     const indexOfBookImage = favoriteBooks.indexOf(bookImageDataId);
                     favoriteBooks.splice(indexOfBookImage, 1);
+                    console.log('test10')
                 }
-            });
-        }
+            }
+        });
     }
+
+
 
 
     render();
     initActions();
-
-
 
 }
